573438bd14566bf5a58f68be841a540e794698fd
Rafał Kitta <rafalkit@gmail.com>
2016-11-23 22:09:55 +0100

    Reformat

---
+++ NeuralNetwork/NeuralNetwork/Layer.swift
@@ -0,0 +1,45 @@
+//
+//  Layer.swift
+//  NeuralNetwork
+//
+//  Created by Rafal Kitta on 23/11/2016.
+//  Copyright © 2016 Rafal Kitta. All rights reserved.
+//
+
+import Foundation
+
+
+// Neural network layer structure
+struct Layer {
+    
+    /// Perceptron
+    var perceptron: Perceptron
+    
+    /// Values vector with values count equal to perceptron culumns number
+    /// - Note: Defaultly initialized with values: 0.0
+    lazy var values: [Double] = [Double](repeating: Double(), count: self.perceptron.n)
+    
+    
+    /// Defult initializer
+    ///
+    /// - parameter perceptron: `Perceptron` instance
+    ///
+    /// - returns: `Layer` instance
+    init(perceptron: Perceptron) {
+        // Store perceptron
+        self.perceptron = perceptron
+        // Randomize perceptron values with range -5...5
+        self.perceptron.randomizeMatrix()
+    }
+}
+
+
+// MARK: - CustomStringConvertible
+extension Layer: CustomStringConvertible {
+    
+    /// Structure instance description format
+    var description: String {
+        return "\(perceptron)"
+    }
+}
+
---
+++ NeuralNetwork/NeuralNetwork/TrainingData.swift
@@ -0,0 +1,20 @@
+//
+//  TrainingData.swift
+//  NeuralNetwork
+//
+//  Created by Rafal Kitta on 23/11/2016.
+//  Copyright © 2016 Rafal Kitta. All rights reserved.
+//
+
+import Foundation
+
+
+/// Neural network training data structure
+struct TrainingData {
+    
+    /// Input training data vector
+    var vectorIn: [Double] = []
+    
+    /// Output training data vector
+    var vectorOut: [Double] = []
+}
