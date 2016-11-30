95ab198eb01bfff72e793740495b35d7dcb87bcc
Rafał Kitta <rafalkit@gmail.com>
2016-12-01 00:07:39 +0100

    Add normalized data source with Heberman survived data set from CV file, Run back propagation for data source

---
+++ source/NeuralNetwork/DataSource.swift
@@ -0,0 +1,45 @@
+//
+//  DataSource.swift
+//  NeuralNetwork
+//
+//  Created by Rafal Kitta on 30/11/2016.
+//  Copyright © 2016 Rafal Kitta. All rights reserved.
+//
+
+import Foundation
+
+
+struct HebermanSurvivalDataSet {
+    var samples: [HebermanSurvivalSample] = []
+    
+    init() {
+        if let path = Bundle.main.path(forResource: "normalized_data", ofType: "csv") {
+            do {
+                let straingData = try String(contentsOfFile: path, encoding: .utf8)
+                let csv = CSwiftV(with: straingData, separator: ";", headers: [String](repeating: "", count: 5))
+                
+                // Iterate over csv rows
+                for row in csv.rows {
+                    let sample = HebermanSurvivalSample(age: Double(row[0])!, surgeryYear: Double(row[1])!, positiveAuxiliaryNodes: Double(row[2])!, died: Double(row[3])!, survive: Double(row[4])!)
+                    samples.append(sample)
+                }
+                
+            } catch {
+                print("Error with reading file: \(error)")
+            }
+        }
+    }
+}
+
+
+
+struct HebermanSurvivalSample {
+    var age: Double
+    var surgeryYear: Double
+    var positiveAuxiliaryNodes: Double
+    var died: Double
+    var survive: Double
+}
+
+
+
